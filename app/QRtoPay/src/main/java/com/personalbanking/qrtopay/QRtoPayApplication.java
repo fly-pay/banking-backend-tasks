13d1176356b6051c62249bea1c6621a530bb0672
hlyamhtetkyaw <hlan1559@gmail.com>
2025-11-19 13:12:08 +0630

    [INIT] first commit

---
+++ app/QRtoPay/src/main/java/com/personalbanking/qrtopay/QRtoPayApplication.java
@@ -0,0 +1,13 @@
+package com.personalbanking.qrtopay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QRtoPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QRtoPayApplication.class, args);
+    }
+
+}
